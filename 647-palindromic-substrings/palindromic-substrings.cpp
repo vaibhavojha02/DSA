092cc5d34e5d6114f35e81cf123ab8f67b479bbd
vaibhav ojha <105600465+vaibhavojha02@users.noreply.github.com>
2025-01-10 21:08:05 +0530

    Time: 1990 ms (5.02%) | Memory: 419.1 MB (7.68%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,20 +1,21 @@
 class Solution {
+    int dp[1001][1001];
     bool isPallindrome(int i, int j, string s) {
-        string t = s.substr(i, j - i + 1);
-        int start = 0, e = t.size() - 1;
-        while (start <= e) {
-            if (t[start] == t[e]) {
-                start++;
-                e--;
-            } else {
-                return false;
-            }
-        }
-        return true;
+       if(i>j){
+         return true;
+       }
+       if(dp[i][j]!=-1){
+        return dp[i][j];
+       }
+       if(s[i]==s[j]){
+         return dp[i][j] =  isPallindrome(i+1,j-1,s);
+       }
+       return dp[i][j] = false;
     }
 
 public:
     int countSubstrings(string s) {
+        memset(dp,-1,sizeof(dp));
         int count = 0;
         for (int i = 0; i < s.size(); i++) {
             for (int j = i; j < s.size(); j++) {
