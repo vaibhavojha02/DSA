df8bad314783503288d8fec1f05ca48a4265d9fa
vaibhav ojha <105600465+vaibhavojha02@users.noreply.github.com>
2025-01-13 18:49:38 +0530

    Time: 0 ms (100.00%) | Memory: 30.8 MB (61.83%) - LeetSync

---
+++ 665-non-decreasing-array/non-decreasing-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        int count = 0;
+        int n = nums.size();
+        if(n==1)return true;
+        for (int i = 1; i < n - 1; i++) {
+            if (i+2<n && nums[i] >= nums[i-1] && nums[i + 1] < nums[i] && nums[i + 1] < nums[i - 1] && nums[i+2] <= nums[i])count++;
+                
+            if (nums[i] < nums[i - 1]) {
+                count++;
+            }
+        }
+        if (nums[n - 1] < nums[n - 2])
+            count++;
+        return (count > 1) ? false : true;
+    }
+};
