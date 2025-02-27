1b7fb938c839d60ed675715bd0051a8cf91b4753
vaibhav ojha <105600465+vaibhavojha02@users.noreply.github.com>
2025-02-27 22:30:58 +0530

    Time: 14 ms (78.28%) | Memory: 27.1 MB (24.48%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>>ans;
+        unordered_map<string,vector<string>>mp;
+        for(auto x: strs)
+        {
+             string temp = x;
+             sort(x.begin(),x.end());
+             mp[x].push_back(temp);
+        }
+         for(auto x:mp){
+             vector<string>temp=x.second;
+              ans.push_back(temp);
+         }
+        return ans;
+    }
+};
