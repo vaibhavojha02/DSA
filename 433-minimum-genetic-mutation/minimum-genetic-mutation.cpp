72042f58c506937123c10dc189a4443823a7b4f8
vaibhav ojha <105600465+vaibhavojha02@users.noreply.github.com>
2025-03-02 12:58:54 +0530

    Time: 3 ms (3.63%) | Memory: 8.9 MB (58.11%) - LeetSync

--- 433-minimum-genetic-mutation/minimum-genetic-mutation.cpp
+++ 433-minimum-genetic-mutation/minimum-genetic-mutation.cpp
@@ -5,35 +5,36 @@ public:
         unordered_set<string> st(bank.begin(), bank.end());
         unordered_set<string> visited;
         visited.insert(start);
-        queue<pair<string, int>> q;
-        q.push({start, 0});
-        int ans = INT_MAX;
+        queue<string> q;
+        q.push(start);
         int level = 0;
         while (!q.empty()) {
 
-            string temp = q.front().first;
-            int currlevel = q.front().second;
-            q.pop();
-            if (temp == end) {
-                ans = min(currlevel, ans);
-            }
+            int n = q.size();
+            while (n--) {
+                string temp = q.front();
+                q.pop();
+                if (temp == end)
+                    return level;
 
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 8; j++) {
-                    string neigbour = temp;
-                    if (neigbour[j] != arr[i]) {
+                for (int i = 0; i < 4; i++) {
+                    for (int j = 0; j < 8; j++) {
+                        string neigbour = temp;
+                        if (neigbour[j] != arr[i]) {
 
-                        neigbour[j] = arr[i];
-                        if (visited.find(neigbour) == visited.end() &&
-                            st.find(neigbour) != st.end()) {
-                            q.push({neigbour, currlevel + 1});
-                            visited.insert(neigbour);
+                            neigbour[j] = arr[i];
+                            if (visited.find(neigbour) == visited.end() &&
+                                st.find(neigbour) != st.end()) {
+                                q.push(neigbour);
+                                visited.insert(neigbour);
+                            }
+                            
                         }
                     }
                 }
             }
+                level++;
         }
-        return ans == INT_MAX ? -1 : ans;
+        return -1;
     }
 };
