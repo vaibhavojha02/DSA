ac131ff3caeacbdf8d740568447195c1bc06eb06
vaibhav ojha <105600465+vaibhavojha02@users.noreply.github.com>
2025-03-03 21:04:25 +0530

    Time: 734 ms (17.43%) | Memory: 482.1 MB (14.42%) - LeetSync

--- 648-replace-words/replace-words.cpp
+++ 648-replace-words/replace-words.cpp
@@ -1,48 +1,24 @@
 class Solution {
 public:
+    string findroot(string& word,unordered_set<string>&st){
+        for(int l=1;l<=word.length();l++){
+            string root =  word.substr(0,l);
+            if(st.find(root)!=st.end()){
+                return root;
+            }
+        }
+        return word;
+    }
     string replaceWords(vector<string>& dictionary, string sentence) {
-        unordered_map<string, int> mp;
-        for (int i = 0; i < dictionary.size(); i++) {
-
-            mp[dictionary[i]]++;
+        unordered_set<string>st(dictionary.begin(),dictionary.end());
+        stringstream ss(sentence);
+        string word;
+        string result;
+        while(getline(ss,word,' ')){
+            result += findroot(word,st) + " ";
         }
-        string ans = "";
-        int i = 0;
-        while (i < sentence.size()) {
-            if (sentence[i] != ' ') {
-                int j = i;
-                int count = 1;
-                bool flag = false;
-                string temp = "";
-                while (sentence[j] != ' ' && j < sentence.size()) {
-                    if (flag == true) {
-                        j++;
-                        continue;
-                    }
+        result.pop_back();
+        return result;
 
-                    temp = sentence.substr(i, count);
-                    if (mp.find(temp) != mp.end()) {
-                        temp += ' ';
-                        ans += temp;
-
-                        flag = true;
-                    }
-
-                    j++;
-                    count++;
-                }
-                i = j + 1;
-                if (flag == false) {
-                    temp += ' ';
-                    ans += temp;
-                }
-            }
-
-            else {
-                i++;
-            }
-        }
-        ans.pop_back();
-        return ans;
     }
 };
