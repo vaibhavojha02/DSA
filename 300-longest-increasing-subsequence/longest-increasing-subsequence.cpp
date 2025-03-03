7bf3905e523b7b83847addb99efc8d7cc4a37546
vaibhav ojha <105600465+vaibhavojha02@users.noreply.github.com>
2025-03-03 23:20:15 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (94.45%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        int ans = 1;
-        int n = nums.size();
-        vector<int> dp(n, 1);
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i]) {
-                    dp[i] = max(dp[i], 1 + dp[j]);
-                }
+    int lengthOfLIS(vector<int>& nums){
+        vector<int>temp;
+        temp.push_back(nums[0]);
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]>temp.back()){
+                temp.push_back(nums[i]);
             }
-            cout<<dp[i]<<" ";
-            ans = max(ans, dp[i]);
+            else{
+                int idx = lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
+                temp[idx] = nums[i];
+            }
         }
-        return ans;
+        return temp.size();
+        
     }
 };
