53b0f5d1a1736b0cdd3ca09d419d4048c1f60bd7
vaibhav ojha <105600465+vaibhavojha02@users.noreply.github.com>
2025-03-12 21:21:59 +0530

    Time: 17 ms (53.06%) | Memory: 14 MB (62.15%) - LeetSync

--- 2340-total-appeal-of-a-string/total-appeal-of-a-string.cpp
+++ 2340-total-appeal-of-a-string/total-appeal-of-a-string.cpp
@@ -1,28 +1,14 @@
 class Solution {
 public:
     long long appealSum(string s) {
-        long long int n = s.size();
-        long long int total = (n * (n + 1)) / 2;
-        vector<long long>substring(26,0LL);
-        for (int i = 0; i < 26; i++) {
-            char ch = 'a' + i;
-            long long int sum = 0LL;
-            long long int cnt = 0LL;
-            for (int j = 0; j < n; j++) {
-                if (s[j] != ch) {
-                    cnt++;
-                } else {
-                    sum += (cnt * (cnt + 1)) / 2;
-                    cnt = 0LL;
-                }
+        vector<int> freq(26, 0);
+        long long res = 0;
+        for (int i = 0; i < s.size(); i++) {
+            freq[s[i] - 'a'] = i + 1;
+            for (int j = 0; j < 26; j++) {
+                res += freq[j];
             }
-            if (cnt != 0)
-                sum += (cnt * (cnt + 1)) / 2;
-            substring[i] = (total - sum);
         }
-        long long result = 0LL;
-
-        for (int i = 0; i < 26; i++)result += substring[i];
-        return result;
+        return res;
     }
 };
