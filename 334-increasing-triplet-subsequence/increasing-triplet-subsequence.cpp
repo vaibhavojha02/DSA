42afe986fe8679acb3b9732b4467079de5072253
vaibhav ojha <105600465+vaibhavojha02@users.noreply.github.com>
2025-03-13 13:31:37 +0530

    Time: 0 ms (100.00%) | Memory: 115.8 MB (21.23%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>arr;
-        arr.push_back(nums[0]);
-        for(int i=1;i<n;i++)
-        {
-            if(arr.size()>=3)return true;
-            if(nums[i]>arr.back()){
-                arr.push_back(nums[i]);
-            }
-            else{
-               int idx = lower_bound(arr.begin(),arr.end(),nums[i])-arr.begin();
-               arr[idx] = nums[i];
-            }
+        int left = INT_MAX, mid = INT_MAX;
+        
+        for (int num : nums) {
+            if (num <= left) 
+                left = num;  // Smallest number found
+            else if (num <= mid) 
+                mid = num;   // Second smallest number found
+            else 
+                return true; // A number greater than 'mid' means an increasing triplet exists
         }
-        if(arr.size()>=3)return 1;
+        
         return false;
     }
 };
