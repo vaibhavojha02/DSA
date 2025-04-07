aa79e092883b490604370fa2ca9292c51e77e08e
vaibhav ojha <105600465+vaibhavojha02@users.noreply.github.com>
2025-04-07 15:00:26 +0530

    Time: 7 ms (83.09%) | Memory: 23.1 MB (36.68%) - LeetSync

--- 524-longest-word-in-dictionary-through-deleting/longest-word-in-dictionary-through-deleting.cpp
+++ 524-longest-word-in-dictionary-through-deleting/longest-word-in-dictionary-through-deleting.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-    string findLongestWord(string s, vector<string>& dictionary) {
-        sort(dictionary.begin(), dictionary.end());
+    string findLongestWord(string s, vector<string>& words) {
+        unordered_map<int, vector<int>> mp;
+        for (int i = 0; i < s.size(); i++) {
+            mp[s[i]].push_back(i);
+        }
+        sort(words.begin(), words.end());
         int len = 0;
         string ans = "";
-        for (auto str : dictionary)
-        {
-            int i = 0, j = 0;
-            while (i < str.size() && j < s.size())
-            {
-                if (str[i] == s[j]) {
-                    i++;
-                    j++;
-                } else {
-                    j++;
+        for (auto st : words) {
+            int maxi = -1;
+            for (int i = 0; i < st.size(); i++) {
+                int idx =
+                    upper_bound(mp[st[i]].begin(), mp[st[i]].end(), maxi) -
+                    mp[st[i]].begin();
+
+                if (idx == mp[st[i]].size())
+                    break;
+                else {
+                    maxi = mp[st[i]][idx];
                 }
-            }
-            if (i == str.size() && str.size() > len) {
-                ans = str;
-                len = str.size();
-                continue;
+                if (i == st.size() - 1 && len < st.size()) {
+                    ans = st;
+                    len = st.size();
+                }
             }
         }
         return ans;
