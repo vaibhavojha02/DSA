7af8b9c9a92b3efe37cbd088c1c636b4a5c2cfb0
vaibhav ojha <105600465+vaibhavojha02@users.noreply.github.com>
2025-04-07 22:16:07 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (49.45%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
@@ -5,16 +5,17 @@ public:
         int totSum = accumulate(nums.begin(), nums.end(), 0);
         int s1 = 0, s2 = 0;
         int ans1 = INT_MIN, ans2 = INT_MAX;
+        //kadanes for maximum sum
         for (int i = 0; i < nums.size(); i++) {
             s1 += nums[i];
             ans1 = max(ans1,s1);
             if(s1<0)s1 = 0;
         }
+        //kadanes for minimum sum
         for (int i = 0; i < nums.size(); i++) {
             s2 += nums[i];
             ans2 = min(ans2, s2);
-            if (s2 > 0)
-                s2 = 0;
+            if (s2 > 0)s2 = 0;
         }
         return ans1 > 0 ? max(ans1, totSum - ans2) : ans1;
     }
