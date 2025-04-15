70775c95a78d71f6389b42152c177ef799f04bb2
vaibhav ojha <105600465+vaibhavojha02@users.noreply.github.com>
2025-04-15 22:13:14 +0530

    Time: 0 ms (100.00%) | Memory: 21.3 MB (89.38%) - LeetSync

--- 1008-binary-tree-cameras/binary-tree-cameras.cpp
+++ 1008-binary-tree-cameras/binary-tree-cameras.cpp
@@ -6,30 +6,34 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    // 0 for children is having camera
-    //-1 children need camera
-    // 1  children is covered think about yourself
-    int dfs(TreeNode* root,int& cameras){
-        if(root==nullptr)return 1;
-        int left = dfs(root->left,cameras);
-        int right = dfs(root->right,cameras);
-        if(left==-1 || right==-1)
-        {
+    // 0 for children is having camera                "camera"
+    //-1  children need camera                         "need"
+    // 1  children is covered think about yourself    "covered"
+    int dfs(TreeNode* root, int& cameras) {
+        if (root == nullptr)
+            return 1;
+        int left = dfs(root->left, cameras);
+        int right = dfs(root->right, cameras);
+        // three is need of camera so we will greedily rovide it
+        if (left == -1 || right == -1) {
             cameras++;
             return 0;
-        }//three is need of camera so we will greedily rovide it
-        if(left == 0 || right== 0)return 1;
+        } 
+        //both left and right child are having camera so current node is coverd
+        if (left == 0 || right == 0)
+            return 1;
+        // no neither left is having camera nor right is having camera and both are covered
+        //which means we are in need of camera
         return -1;
-
     }
     int minCameraCover(TreeNode* root) {
         int cameras = 0;
-        return dfs(root,cameras) == -1 ? cameras + 1 : cameras;
-        
+        return dfs(root, cameras) == -1 ? cameras + 1 : cameras;
     }
 };
