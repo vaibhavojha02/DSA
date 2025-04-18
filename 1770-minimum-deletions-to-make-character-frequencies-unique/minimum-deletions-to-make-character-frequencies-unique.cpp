349d903d6ea017a5896f33801a23b02436be5035
vaibhav ojha <105600465+vaibhavojha02@users.noreply.github.com>
2025-04-18 23:37:30 +0530

    Time: 23 ms (27.56%) | Memory: 20.8 MB (13.28%) - LeetSync

---
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int minDeletions(string s) {
+        vector<int> freq(26, 0);
+        for (int i = 0; i < s.size(); i++) {
+            freq[s[i] - 'a']++;
+        }
+        sort(freq.rbegin(), freq.rend());
+        int ops = 0;
+        for (auto i : freq) {
+            cout << i << " ";
+        }
+        cout<<endl;
+        int number_to_be_present = freq[0] - 1;
+        for (int i = 1; i < 26; i++) {
+            if (freq[i] == 0)
+                break;
+            cout<<number_to_be_present<<endl;
+            if(number_to_be_present<0){
+                ops++;
+                continue;
+            }
+            
+            if (freq[i] != number_to_be_present) {
+                if (freq[i] <= number_to_be_present){
+                    number_to_be_present = max(freq[i] - 1,0);
+                } else {
+                    ops += abs(freq[i] - number_to_be_present);
+                    number_to_be_present = max(0,number_to_be_present-1);
+                }
+            }
+            else{
+                number_to_be_present = max(0,number_to_be_present-1);
+            }
+        }
+        return ops;
+    }
+};
