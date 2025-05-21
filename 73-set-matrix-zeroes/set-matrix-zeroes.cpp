b7344605c34809ae0ed17f2243e3918bf5aa05d0
vaibhav ojha <105600465+vaibhavojha02@users.noreply.github.com>
2025-05-21 12:01:40 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (52.14%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<pair<int,int>>temp;
-        int n = matrix.size(), m = matrix[0].size();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (matrix[i][j] == 0) {
-                    temp.push_back({i,j});
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<int> row(n,0);
+        vector<int> col(m,0);
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+               if(matrix[i][j]==0){
+                  row[i] = 1;
+                  col[j] = 1;
+               }
+            }
+        }
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(row[i]==1 || col[j]==1){
+                    matrix[i][j] = 0;
                 }
             }
         }
-        for(int i = 0;i<temp.size();i++){
-            int row = temp[i].first;
-            int col = temp[i].second;
-            for(int j=0;j<m;j++){
-                matrix[row][j] = 0;
-            }
-            for(int j = 0 ;j<n;j++){
-                matrix[j][col] = 0;
-            }
-        }
         return ;
-        
     }
 };
